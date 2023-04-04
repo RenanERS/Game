55bf28d7ee1d45c946a87fa537af7d59c21405f2
Renan <renansuguimoto@gmail.com>
2023-04-03 21:36:29 -0300

    'game'

---
+++ RPGzinho 1.3.1/WEB/game.js
@@ -0,0 +1,24 @@
+let vida1 = 350
+let ataque1 = 130
+let defesa1 = 30
+let pocoesvida = 3
+let vida2 = 825
+let ataque2 = 70
+let defesa2 = 90
+let trigger1 = 0
+let trigger2 = 0
+// let sorteador = random.randint (1, 20)
+// let sorteador2 = random.randint (1, 150)
+// let sorteador3 = random.randint (1, 1500)
+
+let button 
+let input 
+
+
+document.addEventListener('click', )
+
+function onlclick(){
+    let input = document.getElementById('input').value
+    let button = document.getElementById('button')
+
+    }
